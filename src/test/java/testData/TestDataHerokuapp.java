844858fbaeda03755bafbc4cc464402ee0d63e0e
Sezen <aysezen6@gmail.com>
2023-02-09 16:04:31 +0100

    09.02.2023

--- src/test/java/testData/TestDataHerokuapp.java
+++ src/test/java/testData/TestDataHerokuapp.java
@@ -1,8 +1,12 @@
 package testData;
 
+import org.json.JSONObject;
+
 import java.util.HashMap;
 
 public class TestDataHerokuapp {
+
+    public int statusCode = 200;
 
     /*
     {
@@ -52,6 +56,39 @@ public class TestDataHerokuapp {
         expBodyMap.put("booking",bookingOlusturMap());
 
         return expBodyMap;
+    }
+    public JSONObject innerBodyOlusturJson(){
+
+        JSONObject innerBody = new JSONObject();
+
+        innerBody.put("checkin","2021-06-01");
+        innerBody.put("checkout","2021-06-10");
+
+        return innerBody;
+    }
+
+    public JSONObject requestBodyOlusturJson(){
+
+        JSONObject reqBody = new JSONObject();
+
+        reqBody.put("firstname" , "Ali");
+        reqBody.put("lastname" , "Bak");
+        reqBody.put("totalprice" , 500);
+        reqBody.put("depositpaid" ,false);
+        reqBody.put("bookingdates" , innerBodyOlusturJson());
+        reqBody.put("additionalneeds" , "wi-fi");
+
+        return reqBody;
+    }
+
+    public JSONObject expectedBodyOlusturJson(){
+
+        JSONObject expBody = new JSONObject();
+
+        expBody.put("bookingid",24);
+        expBody.put("booking",requestBodyOlusturJson());
+
+        return expBody;
     }
 
 }
